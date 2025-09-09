42e5acadcd0ddcf13b33970207b2f54015e11f38
influence20 <donblinksdan@gmail.com>
2025-09-09 03:04:24 +0100

    Update payouts.ts

--- src/routes/payouts.ts
+++ src/routes/payouts.ts
@@ -1,12 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { Payout } from '../models/Payout';
-import { auth } from '../middleware/auth';
+import auth from '../middleware/auth';
 import { logger } from '../utils/logger';
+import { AuthRequest } from '../types';
 
 const router = express.Router();
 
 // Get all payouts for user
-router.get('/', auth, async (req, res) => {
+router.get('/', auth, async (req: AuthRequest, res: Response) => {
   try {
     const payouts = await Payout.find({ userId: req.user?._id })
       .populate('investmentId')
